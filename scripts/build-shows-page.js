0fd7a046770a7cc789822a755101271b60f4c61e
Fiorella Marky <fiorella.marky@gmail.com>
2024-10-08 16:44:38 -0400

    Add Shows page, finish mobile styling for dynamic elements, add scripts with html templates

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,17 @@
+{
+  /* <li class="show">
+  <div class="show__column">
+    <span class="show__label">Date</span>
+    <span class="show__info show__info--main">Temp 09 09 2024</span>
+  </div>
+  <div class="show__column">
+    <span class="show__label">Venue</span>
+    <span class="show__info">Temp Venue</span>
+  </div>
+  <div class="show__column">
+    <span class="show__label">Location</span>
+    <span class="show__info">Temp City, CA</span>
+  </div>
+  <button class="show__button">Buy Tickets</button>
+</li>; */
+}
---
+++ scripts/index-page.js
@@ -0,0 +1,14 @@
+{
+  /* <div class="comment__container">
+  <div class="avatar">
+    <img src="" alt="" class="avatar__image" />
+  </div>
+  <div class="comment">
+    <div class="comment__top">
+      <h3 class="comment__name">Fiorella</h3>
+      <span class="comment__date">294893284</span>
+    </div>
+    <p class="comment__content">Hi everyone lalalala</p>
+  </div>
+</div>; */
+}
